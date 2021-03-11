ffbae89e60820db32e65496efadc15776cfd3fb6
mbrayton27 <mbrayton27@gmail.com>
2021-03-11 09:02:45 -0800

    tests added

--- test_calculator.py
+++ test_calculator.py
@@ -2,16 +2,16 @@
 Unit testing the calculator app
 """
 
-import unittest
 import calculator
 
 
-class TestCalculator(unittest.TestCase):
+class TestCalculator:
+
     def test_add(self):
         assert 5 == calculator.add(1, 4)
 
     def test_subtract(self):
         assert 2 == calculator.subtract(5, 3)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_multiply(self):
+        assert 2 == calculator.multiply(1, 2)
